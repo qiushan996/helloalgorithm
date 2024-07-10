0cdf44806dd4af0f7f75fe7882040e391aa11212
0XCC <crazyshine@qq.com>
2024-07-10 23:22:09 +0800

    update: CMake Tutorial Step5, add CTest support

--- CmakeTutorial/Step5/tutorial.cpp
+++ CmakeTutorial/Step5/tutorial.cpp
@@ -9,9 +9,18 @@ int main(int argc, char* argv[]){
   std::cout<<"case1: "<<std::endl;
   toString(str);
 
-  int case2 = 81;
-  std::cout<<"case 2: "<<std::endl;
-  double sqrtcase2 = ssqrt(case2);
-  cout<<"The square root of  "<<case2<<" is: "<<sqrtcase2<<endl;
+  try{
+    int case2 = stoi(argv[1]);
+    std::cout<<"case 2: "<<std::endl;
+    double sqrtcase2 = ssqrt(case2);
+    cout<<case2<<" is "<<sqrtcase2<<endl;
+
+  }catch (const std::invalid_argument& e) {
+        std::cerr << "Invalid argument: " << argv[1] << " is not a valid number" << std::endl;
+        return 1;
+  } catch (const std::out_of_range& e) {
+      std::cerr << "Out of range: " << argv[1] << " is too large" << std::endl;
+      return 1;
+  }
   return 0;
 }
